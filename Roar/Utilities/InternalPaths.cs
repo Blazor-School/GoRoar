7237ca3e4bbd3cede68c85b9982a9ae39116a1cb
Huy Tran <huy.tran@blazorschool.com>
2026-01-04 22:58:18 +0700

    Implement button variant

---
+++ Roar/Utilities/InternalPaths.cs
@@ -0,0 +1,8 @@
+﻿namespace Roar.Utilities;
+
+internal static class InternalPaths
+{
+    public const string BaseWebAwesomePath = "/_content/Roar/webawesome-3.1.0";
+
+    public static string ComposeWebAwesomeComponentJsPath(string componentName) => string.Join("/", BaseWebAwesomePath, "components", componentName, $"{componentName}.js");
+}
---
+++ Roar/Utilities/Variant.cs
@@ -0,0 +1,25 @@
+﻿namespace Roar.Utilities;
+
+public readonly struct Variant : IEquatable<Variant>
+{
+    public string Value => field ?? "neutral";
+
+    public Variant(string value) => Value = string.IsNullOrEmpty(value) ? "neutral" : value;
+
+    public static readonly Variant Neutral = new("neutral");
+    public static readonly Variant Brand = new("brand");
+    public static readonly Variant Success = new("success");
+    public static readonly Variant Warning = new("warning");
+    public static readonly Variant Danger = new("danger");
+
+    public override string ToString() => Value;
+    public bool Equals(Variant other) => Value == other.Value;
+    public override bool Equals(object? obj) => obj is Variant v && Equals(v);
+    public override int GetHashCode() => Value.GetHashCode();
+
+    public static implicit operator Variant(string value) => new(value);
+    public static implicit operator string(Variant v) => v.Value;
+
+    public static bool operator ==(Variant left, Variant right) => left.Equals(right);
+    public static bool operator !=(Variant left, Variant right) => !(left == right);
+}
