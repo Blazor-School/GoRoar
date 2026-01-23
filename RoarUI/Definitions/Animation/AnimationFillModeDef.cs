0b6860fa2f1d587082cb3ebb3fedb94046d285b4
Huy Tran <huy.tran@blazorschool.com>
2026-01-23 09:25:30 +0700

    Finished animation component

---
+++ RoarUI/Definitions/Animation/AnimationFillModeDef.cs
@@ -0,0 +1,13 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Animation;
+
+[StringEnum("FillMode", "auto")]
+[StringEnumMember("None")]
+[StringEnumMember("Backwards")]
+[StringEnumMember("Forwards")]
+[StringEnumMember("Both")]
+[StringEnumMember("Auto")]
+internal class AnimationFillModeDef
+{
+}
