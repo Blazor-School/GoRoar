8f2e1e2ef03bc6199f87c3e930cce8ce5f399dfe
Huy Tran <huy.tran@blazorschool.com>
2026-02-17 11:06:45 +0700

    Adapt WebAwesome 3.2.1 (#11)

--- RoarUI/Utilities/InternalPaths.cs
+++ RoarUI/Utilities/InternalPaths.cs
@@ -2,7 +2,7 @@
 
 internal static class InternalPaths
 {
-    public const string BaseWebAwesomePath = "/_content/RoarUI/webawesome-3.1.0";
+    public const string BaseWebAwesomePath = "/_content/RoarUI/webawesome-3.2.1";
 
     public static string ComposeWebAwesomeComponentJsPath(string componentName)
     {
--- RoarUI/wwwroot/webawesome-3.1.0/components/icon/library.default.js
+++
@@ -1,8 +0,0 @@
-/*! Copyright 2026 Fonticons, Inc. - https://webawesome.com/license */
-import {
-  library_default_default
-} from "../../chunks/chunk.ZGNMW6T7.js";
-import "../../chunks/chunk.AIIMJL75.js";
-export {
-  library_default_default as default
-};
