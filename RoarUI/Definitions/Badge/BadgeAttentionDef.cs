33db4cc332f804aaa6bf72b5550b9d7208184765
Huy Tran <huy.tran@blazorschool.com>
2026-03-01 10:49:52 +0700

    Complete badge component

---
+++ RoarUI/Definitions/Badge/BadgeAttentionDef.cs
@@ -0,0 +1,11 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Badge;
+
+[StringEnum("BadgeAttention", "none")]
+[StringEnumMember("Bounce", "bounce")]
+[StringEnumMember("Pulse", "pulse")]
+[StringEnumMember("None", "none")]
+internal class BadgeAttentionDef
+{
+}
