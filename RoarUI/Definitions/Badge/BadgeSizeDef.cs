277bc9b3740170cd3c2ba957101708d6f9d6f0a1
Huy Tran <huy.tran@blazorschool.com>
2026-03-01 15:07:39 +0700

    Add badge component (#15)

---
+++ RoarUI/Definitions/Badge/BadgeSizeDef.cs
@@ -0,0 +1,13 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Badge;
+
+[StringEnum("BadgeSize", "var(--wa-font-size-xs)")]
+[StringEnumMember("XS", "var(--wa-font-size-xs)")]
+[StringEnumMember("S", "var(--wa-font-size-s)")]
+[StringEnumMember("M", "var(--wa-font-size-m)")]
+[StringEnumMember("L", "var(--wa-font-size-l)")]
+[StringEnumMember("XL", "var(--wa-font-size-xl)")]
+internal class BadgeSizeDef
+{
+}
