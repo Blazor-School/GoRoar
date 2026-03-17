a1c05c1e8303a56e6d90e263ed9f8e79750b915e
Huy Tran <huy.tran@blazorschool.com>
2026-03-17 17:18:48 +0700

    Add dropdown component

---
+++ RoarUI/Definitions/Dropdown/DropdownPlacementDef.cs
@@ -0,0 +1,20 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Dropdown;
+
+[StringEnumMember("top")]
+[StringEnumMember("Top", "top-start")]
+[StringEnumMember("TopEnd", "top-end")]
+[StringEnumMember("Bottom")]
+[StringEnumMember("BottomStart", "bottom-start")]
+[StringEnumMember("BottomEnd", "bottom-end")]
+[StringEnumMember("Right")]
+[StringEnumMember("RightStart", "right-start")]
+[StringEnumMember("RightEnd", "right-end")]
+[StringEnumMember("Left")]
+[StringEnumMember("LeftStart", "left-start")]
+[StringEnumMember("LeftEnd", "left-end")]
+[StringEnum("DropdownPlacement", "bottom-start")]
+internal class DropdownPlacementDef
+{
+}
---
+++ RoarUI/Definitions/Dropdown/DropdownSizeDef.cs
@@ -0,0 +1,11 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Dropdown;
+
+[StringEnumMember("Small")]
+[StringEnumMember("Medium")]
+[StringEnumMember("Large")]
+[StringEnum("DropdownSize", "medium")]
+internal class DropdownSizeDef
+{
+}
