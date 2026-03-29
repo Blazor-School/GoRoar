ef1b5d1fa273141e74b42006639183d6f301db1f
Huy Tran <huy.tran@blazorschool.com>
2026-03-29 17:44:22 +0700

    Add divider (#27)

---
+++ RoarUI/Definitions/Divider/DividerOrientationDef.cs
@@ -0,0 +1,10 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Divider;
+
+[StringEnumMember("Vertical")]
+[StringEnumMember("Horizontal")]
+[StringEnum("DividerOrientation", "horizontal")]
+internal class DividerOrientationDef
+{
+}
