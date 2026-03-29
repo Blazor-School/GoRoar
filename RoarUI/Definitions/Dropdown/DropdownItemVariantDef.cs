dc3d95c0cc06f0f3f252ef22e6893c1c6ae0e8fc
Huy Tran <huy.tran@blazorschool.com>
2026-03-29 10:45:33 +0700

    Add dropdown component (#25)

---
+++ RoarUI/Definitions/Dropdown/DropdownItemVariantDef.cs
@@ -0,0 +1,10 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Dropdown;
+
+[StringEnumMember("Danger")]
+[StringEnumMember("Default")]
+[StringEnum("DropdownItemVariant", "default")]
+internal class DropdownItemVariantDef
+{
+}
