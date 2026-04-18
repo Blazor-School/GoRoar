423764a44a45c905db17dd8fda86c94ac4829671
Huy Tran <huy.tran@blazorschool.com>
2026-04-18 13:26:40 +0700

    Render all properties

--- RoarUI/Services/ThemeService.cs
+++ RoarUI/Services/ThemeService.cs
@@ -6,12 +6,18 @@ namespace RoarUI;
 public class ThemeService(IHttpClientFactory HttpClientFactory)
 {
     internal RoarTheme? CustomTheme { get; set; }
+    public event EventHandler<RoarTheme>? OnThemeLoaded;
 
     public async Task LoadThemeAsync(string path)
     {
         var httpClient = HttpClientFactory.CreateClient();
         CustomTheme = await httpClient.GetFromJsonAsync<RoarTheme>(path);
+        OnThemeLoaded?.Invoke(this, CustomTheme);
     }
 
-    public void LoadTheme(RoarTheme theme) => CustomTheme = theme;
+    public void LoadTheme(RoarTheme theme)
+    {
+        CustomTheme = theme;
+        OnThemeLoaded?.Invoke(this, theme);
+    }
 }
--- RoarUI/Theme/Shadow.cs
+++ RoarUI/Theme/Shadow.cs
@@ -2,19 +2,19 @@
 
 public class Shadow
 {
-    public string ShadowS { get; set; } = "var(--wa-shadow-offset-x-s) var(--wa-shadow-offset-y-s) var(--wa-shadow-blur-s) var(--wa-shadow-spread-s) var(--wa-color-shadow)";
-    public string ShadowM { get; set; } = "var(--wa-shadow-offset-x-m) var(--wa-shadow-offset-y-m) var(--wa-shadow-blur-m) var(--wa-shadow-spread-m) var(--wa-color-shadow)";
-    public string ShadowL { get; set; } = "var(--wa-shadow-offset-x-l) var(--wa-shadow-offset-y-l) var(--wa-shadow-blur-l) var(--wa-shadow-spread-l) var(--wa-color-shadow)";
-    public string ShadowOffsetXS { get; set; } = "calc(var(--wa-shadow-offset-x-scale) * 0.125rem)";
-    public string ShadowOffsetXM { get; set; } = "calc(var(--wa-shadow-offset-x-scale) * 0.25rem)";
-    public string ShadowOffsetXL { get; set; } = "calc(var(--wa-shadow-offset-x-scale) * 0.5rem)";
-    public string ShadowOffsetYS { get; set; } = "calc(var(--wa-shadow-offset-y-scale) * 0.125rem)";
-    public string ShadowOffsetYM { get; set; } = "calc(var(--wa-shadow-offset-y-scale) * 0.25rem)";
-    public string ShadowOffsetYL { get; set; } = "calc(var(--wa-shadow-offset-y-scale) * 0.5rem)";
-    public string ShadowBlurS { get; set; } = "calc(var(--wa-shadow-blur-scale) * 0.125rem)";
-    public string ShadowBlurM { get; set; } = "calc(var(--wa-shadow-blur-scale) * 0.25rem)";
-    public string ShadowBlurL { get; set; } = "calc(var(--wa-shadow-blur-scale) * 0.5rem)";
-    public string ShadowSpreadS { get; set; } = "calc(var(--wa-shadow-spread-scale) * 0.125rem)";
-    public string ShadowSpreadM { get; set; } = "calc(var(--wa-shadow-spread-scale) * 0.25rem)";
-    public string ShadowSpreadL { get; set; } = "calc(var(--wa-shadow-spread-scale) * 0.5rem)";
+    public string S { get; set; } = "var(--wa-shadow-offset-x-s) var(--wa-shadow-offset-y-s) var(--wa-shadow-blur-s) var(--wa-shadow-spread-s) var(--wa-color-shadow)";
+    public string M { get; set; } = "var(--wa-shadow-offset-x-m) var(--wa-shadow-offset-y-m) var(--wa-shadow-blur-m) var(--wa-shadow-spread-m) var(--wa-color-shadow)";
+    public string L { get; set; } = "var(--wa-shadow-offset-x-l) var(--wa-shadow-offset-y-l) var(--wa-shadow-blur-l) var(--wa-shadow-spread-l) var(--wa-color-shadow)";
+    public string OffsetXS { get; set; } = "calc(var(--wa-shadow-offset-x-scale) * 0.125rem)";
+    public string OffsetXM { get; set; } = "calc(var(--wa-shadow-offset-x-scale) * 0.25rem)";
+    public string OffsetXL { get; set; } = "calc(var(--wa-shadow-offset-x-scale) * 0.5rem)";
+    public string OffsetYS { get; set; } = "calc(var(--wa-shadow-offset-y-scale) * 0.125rem)";
+    public string OffsetYM { get; set; } = "calc(var(--wa-shadow-offset-y-scale) * 0.25rem)";
+    public string OffsetYL { get; set; } = "calc(var(--wa-shadow-offset-y-scale) * 0.5rem)";
+    public string BlurS { get; set; } = "calc(var(--wa-shadow-blur-scale) * 0.125rem)";
+    public string BlurM { get; set; } = "calc(var(--wa-shadow-blur-scale) * 0.25rem)";
+    public string BlurL { get; set; } = "calc(var(--wa-shadow-blur-scale) * 0.5rem)";
+    public string SpreadS { get; set; } = "calc(var(--wa-shadow-spread-scale) * 0.125rem)";
+    public string SpreadM { get; set; } = "calc(var(--wa-shadow-spread-scale) * 0.25rem)";
+    public string SpreadL { get; set; } = "calc(var(--wa-shadow-spread-scale) * 0.5rem)";
 }
