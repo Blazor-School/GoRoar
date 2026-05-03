3240a71a1a482bb518afda75e2d827650db0ff72
Huy Tran <huy.tran@blazorschool.com>
2026-05-03 15:01:54 +0700

    Add tooltip show case

--- RoarUI/Infrastructure/StringEnumMemberAttribute.cs
+++ RoarUI/Infrastructure/StringEnumMemberAttribute.cs
@@ -16,36 +16,5 @@
 internal sealed class StringEnumMemberAttribute(string PropertyName, string? Value = null) : Attribute
 {
     public string PropertyName { get; } = PropertyName;
-    public string Value { get; } = string.IsNullOrEmpty(Value) ? ToKebabCase(PropertyName) : Value;
-
-    private static string ToKebabCase(string input)
-    {
-        if (string.IsNullOrEmpty(input))
-        {
-            return input;
-        }
-
-        var result = new System.Text.StringBuilder();
-
-        for (int i = 0; i < input.Length; i++)
-        {
-            char c = input[i];
-
-            if (char.IsUpper(c))
-            {
-                if (i > 0)
-                {
-                    result.Append('-');
-                }
-
-                result.Append(char.ToLowerInvariant(c));
-            }
-            else
-            {
-                result.Append(c);
-            }
-        }
-
-        return result.ToString();
-    }
+    public string? Value { get; } = Value;
 }
