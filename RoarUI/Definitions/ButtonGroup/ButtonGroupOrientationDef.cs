f9ede3824ee97ac48eab40ca5ac943f554a387bb
Huy Tran <huy.tran@blazorschool.com>
2026-05-07 11:47:51 +0700

    Add button group component (#42)

---
+++ RoarUI/Definitions/ButtonGroup/ButtonGroupOrientationDef.cs
@@ -0,0 +1,10 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.ButtonGroup;
+
+[StringEnum("ButtonGroupOrientation", "horizontal")]
+[StringEnumMember("Horizontal")]
+[StringEnumMember("Vertical")]
+internal class ButtonGroupOrientationDef
+{
+}
