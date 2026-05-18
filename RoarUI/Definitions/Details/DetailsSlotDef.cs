5b68f2c949557d4dccf1b4ffd17fb2272c475d70
Huy Tran <huy.tran@blazorschool.com>
2026-05-18 20:54:57 +0700

    Add details component

---
+++ RoarUI/Definitions/Details/DetailsSlotDef.cs
@@ -0,0 +1,11 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Details;
+
+[StringEnum("DetailsSlot")]
+[StringEnumMember("CollapseIcon")]
+[StringEnumMember("ExpandIcon")]
+[StringEnumMember("Summary")]
+internal class DetailsSlotDef
+{
+}
