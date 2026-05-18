fbf8074c38836b68ca45e0d474f72c0007fb4716
Huy Tran <huy.tran@blazorschool.com>
2026-05-18 19:55:54 +0700

    Add comparison component (#43)

---
+++ RoarUI/Definitions/ComparisonSlotDef.cs
@@ -0,0 +1,11 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions;
+
+[StringEnum("ComparisonSlot")]
+[StringEnumMember("After")]
+[StringEnumMember("Before")]
+[StringEnumMember("Handle")]
+internal class ComparisonSlotDef
+{
+}
---
+++ RoarUI/Events/Comparison/ComparisonChangeEventArgs.cs
@@ -0,0 +1,3 @@
+﻿namespace RoarUI.Events;
+
+public record ComparisonChangeEventArgs(double Position);
