a811592a43d58a71d4f584d65ae0ac04ba1aa6fe
Huy Tran <huy.tran@blazorschool.com>
2026-05-19 20:51:36 +0700

    Add details component (#44)

---
+++ RoarUI/Definitions/Details/DetailsAppearanceDef.cs
@@ -0,0 +1,12 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Details;
+
+[StringEnum("DetailsAppearance", "outlined")]
+[StringEnumMember("Filled")]
+[StringEnumMember("Outlined")]
+[StringEnumMember("FilledOutlined")]
+[StringEnumMember("Plain")]
+internal class DetailsAppearanceDef
+{
+}
---
+++ RoarUI/Definitions/Details/DetailsIconPlacementDef.cs
@@ -0,0 +1,10 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Details;
+
+[StringEnum("DetailsIconPlacement", "end")]
+[StringEnumMember("Start")]
+[StringEnumMember("End")]
+internal class DetailsIconPlacementDef
+{
+}
