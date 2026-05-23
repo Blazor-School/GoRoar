91b43360bddd4a9411b8d839daa2a6a777ad2c1c
Huy Tran <huy.tran@blazorschool.com>
2026-05-23 15:54:05 +0700

    Add drawer component (#46)

---
+++ RoarUI/Definitions/Drawer/DrawerPlacementDef.cs
@@ -0,0 +1,12 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Drawer;
+
+[StringEnum("DrawerPlacement", "end")]
+[StringEnumMember("Top")]
+[StringEnumMember("End")]
+[StringEnumMember("Bottom")]
+[StringEnumMember("Start")]
+internal class DrawerPlacementDef
+{
+}
---
+++ RoarUI/Definitions/Drawer/DrawerSlotDef.cs
@@ -0,0 +1,11 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Drawer;
+
+[StringEnum("DrawerSlot")]
+[StringEnumMember("Footer")]
+[StringEnumMember("HeaderActions")]
+[StringEnumMember("Label")]
+internal class DrawerSlotDef
+{
+}
