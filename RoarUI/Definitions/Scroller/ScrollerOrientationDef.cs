e0e22282ce7311a1c16bb2bef71bb96cfeff3f0b
Huy Tran <huy.tran@blazorschool.com>
2026-05-24 11:04:12 +0700

    Add scroller component (#50)

---
+++ RoarUI/Definitions/Scroller/ScrollerOrientationDef.cs
@@ -0,0 +1,10 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Scroller;
+
+[StringEnum("ScrollerOrientation", "horizontal")]
+[StringEnumMember("Horizontal")]
+[StringEnumMember("Vertical")]
+internal class ScrollerOrientationDef
+{
+}
