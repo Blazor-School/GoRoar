695a44ec69e994e5d76ae037ecc50a52bc4eca4a
Huy Tran <huy.tran@blazorschool.com>
2026-05-24 14:14:53 +0700

    Add skeleton component

---
+++ RoarUI/Definitions/Skeleton/SkeletonEffectDef.cs
@@ -0,0 +1,11 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Skeleton;
+
+[StringEnum("SkeletonEffect", "none")]
+[StringEnumMember("Pulse")]
+[StringEnumMember("Sheen")]
+[StringEnumMember("None")]
+internal class SkeletonEffectDef
+{
+}
