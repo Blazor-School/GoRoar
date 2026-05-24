3426d86559ca32f2fd97b31a2452723ccc9cb369
Huy Tran <huy.tran@blazorschool.com>
2026-05-24 10:46:16 +0700

    Add QR Code component (#49)

---
+++ RoarUI/Definitions/QRCode/QRCodeErrorCorrectionDef.cs
@@ -0,0 +1,12 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.QRCode;
+
+[StringEnum("QRCodeErrorCorrection", "H")]
+[StringEnumMember("L", "L")]
+[StringEnumMember("M", "M")]
+[StringEnumMember("Q", "Q")]
+[StringEnumMember("H", "H")]
+internal class QRCodeErrorCorrectionDef
+{
+}
