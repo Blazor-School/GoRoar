99ba4c12a2265b8ff26bfe07063b88bd122041aa
Huy Tran <huy.tran@blazorschool.com>
2026-05-27 16:05:51 +0700

    Add tag component

---
+++ RoarUI/Definitions/Tag/TagAppearanceDef.cs
@@ -0,0 +1,12 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Tag;
+
+[StringEnum("TagAppearance", "filled-outlined")]
+[StringEnumMember("Accent")]
+[StringEnumMember("Filled")]
+[StringEnumMember("Outlined")]
+[StringEnumMember("FilledOutlined")]
+internal class TagAppearanceDef
+{
+}
---
+++ RoarUI/Definitions/Tag/TagVariantDef.cs
@@ -0,0 +1,13 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Tag;
+
+[StringEnum("TagVariant", "neutral")]
+[StringEnumMember("Brand")]
+[StringEnumMember("Neutral")]
+[StringEnumMember("Success")]
+[StringEnumMember("Warning")]
+[StringEnumMember("Danger")]
+internal class TagVariantDef
+{
+}
