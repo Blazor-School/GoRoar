81db1341abade1766807301f28760205e41f9278
Huy Tran <huy.tran@blazorschool.com>
2026-06-02 20:31:13 +0700

    Add zoomable frame (#58)

---
+++ RoarUI/Definitions/ZoomableFrame/ZoomableFrameLoadingDef.cs
@@ -0,0 +1,10 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.ZoomableFrame;
+
+[StringEnum("ZoomableFrameLoading", "eager")]
+[StringEnumMember("Eager")]
+[StringEnumMember("Lazy")]
+internal class ZoomableFrameLoadingDef
+{
+}
