5753f6913c7a71966980df56cb3b22de5f980291
Huy Tran <huy.tran@blazorschool.com>
2026-06-09 14:12:26 +0700

    Add roar checkbox component

---
+++ RoarUI/Definitions/Checkbox/CheckboxSizeDef.cs
@@ -0,0 +1,13 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Checkbox;
+
+[StringEnum("CheckboxSize", "m")]
+[StringEnumMember("Xs")]
+[StringEnumMember("S")]
+[StringEnumMember("M")]
+[StringEnumMember("L")]
+[StringEnumMember("Xl")]
+internal class CheckboxSizeDef
+{
+}
---
+++ RoarUI/Definitions/Checkbox/CheckboxSlotDef.cs
@@ -0,0 +1,9 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Checkbox;
+
+[StringEnum("CheckboxSlot")]
+[StringEnumMember("Hint")]
+internal class CheckboxSlotDef
+{
+}
