6daf686554868e3d77e345f6142e120ce6109f47
Huy Tran <huy.tran@blazorschool.com>
2026-06-21 17:14:08 +0700

    Add more sample to  checkbox group

---
+++ RoarUI.ShowcaseKit/FormModels/Checkbox/CheckboxGroupIcecreamForm.cs
@@ -0,0 +1,9 @@
+﻿using RoarUI.Validations;
+
+namespace RoarUI.ShowcaseKit.FormModels.Checkbox;
+
+public class CheckboxGroupIcecreamForm
+{
+    [CheckboxGroupRequired(ErrorMessage = "Choose at least 1 option")]
+    public List<bool> CheckboxGroup1 { get; set; } = [false, false, false];
+}
---
+++ RoarUI/Validations/Checkbox/CheckboxGroupRequiredAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoarUI.Validations;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+public sealed class CheckboxGroupRequiredAttribute : ValidationAttribute
+{
+    public CheckboxGroupRequiredAttribute() => ErrorMessage = "{0} requires at least one checked option.";
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext) => value switch
+    {
+        null => new ValidationResult(FormatErrorMessage(validationContext.DisplayName), validationContext.MemberName is not null ? [validationContext.MemberName] : null),
+
+        IEnumerable<bool> boolValues => boolValues.Any(x => x) ? ValidationResult.Success : new ValidationResult(FormatErrorMessage(validationContext.DisplayName), validationContext.MemberName is not null ? [validationContext.MemberName] : null),
+
+        _ => new ValidationResult($"{validationContext.DisplayName} must be a collection of boolean values.")
+    };
+}
