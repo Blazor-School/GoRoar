b27658c7b6f19dd6cb764f044ae72272ea043061
Huy Tran <huy.tran@blazorschool.com>
2026-06-21 15:30:27 +0700

    Add description to checkbox parameter

---
+++ RoarUI/Definitions/Checkbox/CheckboxGroupOrientationDef.cs
@@ -0,0 +1,10 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.Checkbox;
+
+[StringEnum("CheckboxGroupOrientation", "vertical")]
+[StringEnumMember("Horizontal")]
+[StringEnumMember("Vertical")]
+internal class CheckboxGroupOrientationDef
+{
+}
