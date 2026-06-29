f51bbb0bf1036582997573d94635ead80f1e9c7b
Huy Tran <huy.tran@blazorschool.com>
2026-06-29 17:30:21 +0700

    Add color picker component

---
+++ RoarUI/Definitions/ColorPicker/ColorPickerPlacementDef.cs
@@ -0,0 +1,20 @@
+﻿using RoarUI.Infrastructure;
+
+namespace RoarUI.Definitions.ColorPicker;
+
+[StringEnum("ColorPickerPlacement", "bottom-start")]
+[StringEnumMember("Top")]
+[StringEnumMember("TopStart")]
+[StringEnumMember("TopEnd")]
+[StringEnumMember("Bottom")]
+[StringEnumMember("BottomStart")]
+[StringEnumMember("BottomEnd")]
+[StringEnumMember("Right")]
+[StringEnumMember("RightStart")]
+[StringEnumMember("RightEnd")]
+[StringEnumMember("Left")]
+[StringEnumMember("LeftStart")]
+[StringEnumMember("LeftEnd")]
+internal class ColorPickerPlacementDef
+{
+}
---
+++ RoarUI/Definitions/ColorPicker/ColorPickerSwatches.cs
@@ -0,0 +1,30 @@
+﻿namespace RoarUI; // Public struct so no deep namespace
+
+public readonly struct ColorPickerSwatches
+{
+    private readonly object? _value;
+
+    private ColorPickerSwatches(object value) => _value = value;
+
+    public static ColorPickerSwatches FromColors(params string[] colors)
+    {
+        ArgumentNullException.ThrowIfNull(colors);
+        string value = string.Join(";", colors.Where(color => !string.IsNullOrWhiteSpace(color)));
+
+        return new(value);
+    }
+
+    public static ColorPickerSwatches FromSwatchList(params ColorPickerSwatch[] swatches)
+    {
+        ArgumentNullException.ThrowIfNull(swatches);
+
+        return new(swatches);
+    }
+
+    public override string ToString() => _value switch
+    {
+        string value => value,
+        ColorPickerSwatch[] swatches => System.Text.Json.JsonSerializer.Serialize(swatches),
+        _ => string.Empty
+    };
+}
